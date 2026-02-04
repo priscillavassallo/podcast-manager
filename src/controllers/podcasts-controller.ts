b0499dbfa02b97ebedd77c513f20a9a717ad14c9
priscillavassallo <priscillavassallo96@gmail.com>
2026-02-04 18:43:07 -0300

    feat: adiciona server e controller

---
+++ src/controllers/podcasts-controller.ts
@@ -0,0 +1,9 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+export const getListEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end({ name: "felipe" });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+import * as http from "http";
+import { getListEpisodes } from "./controllers/podcasts-controller";
+
+const server = http.createServer(
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.method === "GET") {
+      await getListEpisodes(req, res);
+    }
+  },
+);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`servidor iniciado na porta ${port}`);
+});
